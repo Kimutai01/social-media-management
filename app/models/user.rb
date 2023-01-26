1acb76560dee892ebcdab064fe9397781025dde5
Kimutai01 <kiprotichkimutai01@gmail.com>
2023-01-26 16:59:18 +0300

    Added email validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ApplicationRecord
     has_secure_password
+
+    validates :email, presence: true, uniqueness: true, format: { with: /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/ }, message: "Invalid email address"
 end
