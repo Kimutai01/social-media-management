7070b4933c394cd2a5341ed8b23b9d0f2234c63a
Kimutai01 <kiprotichkimutai01@gmail.com>
2023-01-27 07:21:30 +0300

    vite react app

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   get "about", to: "about#index"
 
   root to: "main#index"
+  get "sign_up", to: "registrations#new"
+  post ""
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
